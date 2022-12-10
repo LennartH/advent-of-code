405b630f955eb8a914659dfefb2a4351d2b229cc
Lennart Hensler <lennart@hensler.email>
2022-12-10 12:11:37 +0100

    2022 Day 10 Part 2

--- 2022/src/day-10_cathode-ray-tube.ts
+++ 2022/src/day-10_cathode-ray-tube.ts
@@ -53,26 +53,49 @@ class AddXInstruction extends Instruction {
 }
 
 class Device {
-  readonly register = {
-    X: 1,
-  };
-
   private _cycle = 0;
   get cycle(): number {
     return this._cycle;
   }
 
-  private readonly signalStrengthLog: number[] = [];
+  readonly register = {
+    X: 1,
+  };
 
+  private readonly signalStrengthLog: number[] = [];
   get totalSignalStrength(): number {
     return this.signalStrengthLog.reduce((s, v) => s + v, 0);
   }
 
+  private readonly display: string[][]
+
+  constructor() {
+    this.display = [
+      new Array(40).fill('.'),
+      new Array(40).fill('.'),
+      new Array(40).fill('.'),
+      new Array(40).fill('.'),
+      new Array(40).fill('.'),
+      new Array(40).fill('.'),
+    ]
+  }
+
   step() {
     this._cycle++;
+
     if (this._cycle === 20 || (this._cycle <= 220 && (this._cycle - 20) % 40 === 0)) {
       this.signalStrengthLog.push(this._cycle * this.register.X);
     }
+
+    const displayX = (this._cycle - 1) % 40;
+    const displayY = Math.floor((this._cycle - 1) / 40);
+    if (displayX >= this.register.X - 1 && displayX <= this.register.X + 1) {
+      this.display[displayY][displayX] = '#';
+    }
+  }
+
+  displaySnapshot(): string {
+    return this.display.map((l) => l.map((c) => c+c).join('')).join('\n');
   }
 }
 
@@ -82,8 +105,17 @@ function exampleSolution() {
   lines.map((l) => Instruction.parse(l)).forEach((i) => i.execute(device));
 
   const part1Result = device.totalSignalStrength;
-  const part2Result = undefined;
-  console.log(`Solution for example input: Part 1 ${part1Result} | Part 2 ${part2Result}`);
+  const part2Result = device.displaySnapshot();
+  const separatorLine = new Array(80).fill('-').join('');
+  const expectedPart2Result = `
+####....####....####....####....####....####....####....####....####....####....
+######......######......######......######......######......######......######..
+########........########........########........########........########........
+##########..........##########..........##########..........##########..........
+############............############............############............########
+##############..............##############..............##############..........
+    `.trim();
+  console.log(`Solution for example input\n  Part 1 ${part1Result}\n  Part 2\n${part2Result}\n  Expected for Part 2\n${expectedPart2Result}\n\n${separatorLine}\n`);
 }
 
 function part1Solution() {
@@ -95,7 +127,9 @@ function part1Solution() {
 
 function part2Solution() {
   const lines = fs.readFileSync('./assets/day-10_cathode-ray-tube.input.txt', 'utf-8').trim().split('\n');
-  // console.log(`Solution for Part 2: ${positionVisitedByTail}`);
+  const device = new Device();
+  lines.map((l) => Instruction.parse(l)).forEach((i) => i.execute(device));
+  console.log(`Solution for Part 2:\n${device.displaySnapshot()}`);
 }
 
 
