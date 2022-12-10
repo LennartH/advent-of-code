6052c1bf6a9f2e529e9ea25f55faf21370ce0dfd
Lennart Hensler <lennart@hensler.email>
2022-12-10 11:01:13 +0100

    2022 Day 10 Part 1

---
+++ 2022/src/day-10_cathode-ray-tube.ts
@@ -0,0 +1,104 @@
+import * as fs from 'fs';
+
+abstract class Instruction {
+  readonly name: string;
+  readonly args: unknown[];
+  readonly duration: number;
+
+  static parse(line: string): Instruction {
+    const parts = line.split(' ');
+    if (parts[0] === 'noop') {
+      return new NoopInstruction();
+    }
+    if (parts[0] === 'addx') {
+      return new AddXInstruction(Number(parts[1]));
+    }
+    throw new Error(`Unknown instruction '${parts[0]}'`);
+  }
+
+  protected constructor(name: string, duration: number, ...args: unknown[]) {
+    this.name = name;
+    this.duration = duration;
+    this.args = args;
+  }
+
+  execute(device: Device) {
+    for (let i = 0; i < this.duration; i++) {
+      device.step();
+    }
+    this.doExecute(device);
+  }
+
+  protected abstract doExecute(device: Device): void;
+}
+
+class NoopInstruction extends Instruction {
+  constructor() {
+    super('noop', 1);
+  }
+
+  protected doExecute(device: Device) {}
+}
+
+class AddXInstruction extends Instruction {
+  declare readonly args: number[];
+
+  constructor(value: number) {
+    super('addx', 2, value);
+  }
+
+  protected doExecute(device: Device) {
+    device.register.X += this.args[0];
+  }
+}
+
+class Device {
+  readonly register = {
+    X: 1,
+  };
+
+  private _cycle = 0;
+  get cycle(): number {
+    return this._cycle;
+  }
+
+  private readonly signalStrengthLog: number[] = [];
+
+  get totalSignalStrength(): number {
+    return this.signalStrengthLog.reduce((s, v) => s + v, 0);
+  }
+
+  step() {
+    this._cycle++;
+    if (this._cycle === 20 || (this._cycle <= 220 && (this._cycle - 20) % 40 === 0)) {
+      this.signalStrengthLog.push(this._cycle * this.register.X);
+    }
+  }
+}
+
+function exampleSolution() {
+  const lines = fs.readFileSync('./assets/day-10_cathode-ray-tube.example-input.txt', 'utf-8').trim().split('\n');
+  const device = new Device();
+  lines.map((l) => Instruction.parse(l)).forEach((i) => i.execute(device));
+
+  const part1Result = device.totalSignalStrength;
+  const part2Result = undefined;
+  console.log(`Solution for example input: Part 1 ${part1Result} | Part 2 ${part2Result}`);
+}
+
+function part1Solution() {
+  const lines = fs.readFileSync('./assets/day-10_cathode-ray-tube.input.txt', 'utf-8').trim().split('\n');
+  const device = new Device();
+  lines.map((l) => Instruction.parse(l)).forEach((i) => i.execute(device));
+  console.log(`Solution for Part 1: ${device.totalSignalStrength}`);
+}
+
+function part2Solution() {
+  const lines = fs.readFileSync('./assets/day-10_cathode-ray-tube.input.txt', 'utf-8').trim().split('\n');
+  // console.log(`Solution for Part 2: ${positionVisitedByTail}`);
+}
+
+
+exampleSolution();
+part1Solution();
+part2Solution();
