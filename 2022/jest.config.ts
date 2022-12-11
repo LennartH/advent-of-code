56d3b1ffd0511335887849d8999590dd49bd7e6d
Lennart Hensler <lennart@hensler.email>
2022-12-11 14:41:39 +0100

    2022 Day 11

---
+++ 2022/jest.config.ts
@@ -0,0 +1,10 @@
+/*
+ * For a detailed explanation regarding each configuration property and type check, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
+export default {
+  preset: 'ts-jest',
+  clearMocks: true,
+  collectCoverage: false,
+};
