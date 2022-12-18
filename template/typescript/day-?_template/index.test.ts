c89d3f35827b443b30e86ab6fe6532eb9eed8b7a
Lennart Hensler <lennart@hensler.email>
2022-12-18 10:27:00 +0100

    Add simple typescript template

---
+++ template/typescript/day-?_template/index.test.ts
@@ -0,0 +1,26 @@
+describe('Day ?', () => {
+  describe('example input', () => {
+    const input = `
+    
+    `;
+
+    test('solution is ? for part 1', () => {
+      throw new Error('Not implemented')
+    });
+    test.skip('solution is ? for part 2', () => {
+      throw new Error('Not implemented')
+    });
+  });
+  describe('solution is', () => {
+    const inputPath = `${__dirname}/input`;
+    test('? for part 1', () => {
+      throw new Error('Not implemented')
+    });
+    test.skip('? for part 2', () => {
+      throw new Error('Not implemented')
+    });
+  });
+
+  // region Tests for smaller parts
+  // endregion
+});
