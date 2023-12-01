138ce3761dd690e48594763c53522c7aef91eb50
Lennart Hensler <lennart@hensler.email>
2023-12-01 11:44:03 +0100

    2023 Day 1 Part 2

--- 2023/src/day-01_trebuchet-calibration/index.ts
+++ 2023/src/day-01_trebuchet-calibration/index.ts
@@ -1,13 +1,38 @@
-import { splitLines } from '@util';
+import { revertString, splitLines } from '@util';
 
-export function calculateCalibrationSum(calibrationDocument: string): number {
-  return splitLines(calibrationDocument).map(findCalibrationValue).reduce((s, v) => s + v, 0);
+export function calculateCalibrationSum(calibrationDocument: string, parseWords = false): number {
+  return splitLines(calibrationDocument)
+    .map(v => findCalibrationValue(v, parseWords))
+    .reduce((s, v) => s + v, 0);
 }
 
-function findCalibrationValue(line: string): number {
-  const numbers = line.match(/\d/g)?.map(Number);
-  if (numbers == null || numbers.length === 0) {
+function findCalibrationValue(line: string, parseWords: boolean): number {
+  const firstMatcher = parseWords ? firstDigitWordMatcher : numberMatcher;
+  const lastMatcher = parseWords ? lastDigitWordMatcher : numberMatcher;
+  const toNumber = parseWords ? (v: string) => wordToNumber[v] || Number(v) : Number;
+
+  const firstMatch = line.match(firstMatcher);
+  const lastMatch = revertString(line).match(lastMatcher);
+  if (firstMatch == null || lastMatch == null) {
     throw new Error(`Unable to find calibration value in line: ${line}`);
   }
-  return numbers[0] * 10 + numbers.at(-1)!;
+
+  const firstDigit = firstMatch[0];
+  const lastDigit = revertString(lastMatch[0]);
+  return toNumber(firstDigit) * 10 + toNumber(lastDigit);
 }
+
+const numberMatcher = /\d/;
+const wordToNumber: Record<string, number> = {
+  one: 1,
+  two: 2,
+  three: 3,
+  four: 4,
+  five: 5,
+  six: 6,
+  seven: 7,
+  eight: 8,
+  nine: 9,
+}
+const firstDigitWordMatcher = new RegExp(`\\d|${Object.keys(wordToNumber).join('|')}`, '')
+const lastDigitWordMatcher = new RegExp(`\\d|${Object.keys(wordToNumber).map(revertString).join('|')}`, '')
