a5fe03f0639132d5a591b67cbaa454b64adb0ef4
Lennart Hensler <lennart@hensler.email>
2023-12-01 09:48:17 +0100

    Initial setup for 2023

---
+++ 2023/jest.config.ts
@@ -0,0 +1,22 @@
+import type { JestConfigWithTsJest } from 'ts-jest';
+import { pathsToModuleNameMapper } from 'ts-jest';
+import * as fs from 'fs';
+
+const { compilerOptions } = JSON.parse(fs.readFileSync(`${__dirname}/./tsconfig.json`, 'utf-8'))
+
+/*
+ * For a detailed explanation regarding each configuration property and type check, visit:
+ * https://jestjs.io/docs/configuration
+ */
+const jestConfig: JestConfigWithTsJest = {
+  preset: 'ts-jest',
+  clearMocks: true,
+  collectCoverage: false,
+  setupFilesAfterEnv: ['./test-setup.js'],
+
+  roots: ['<rootDir>'],
+  modulePaths: [compilerOptions.baseUrl],
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, { prefix: '<rootDir>/' }),
+};
+
+export default jestConfig;
