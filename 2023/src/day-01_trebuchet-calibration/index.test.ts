af1703ec1c335f8015a9eb2c4fbe7fc748022ade
Lennart Hensler <lennart@hensler.email>
2023-12-01 11:44:03 +0100

    2023 Day 1 Part 2

--- 2023/src/day-01_trebuchet-calibration/index.test.ts
+++ 2023/src/day-01_trebuchet-calibration/index.test.ts
@@ -3,19 +3,28 @@ import { readFile } from '@util';
 
 describe('Day 1', () => {
   describe('example input', () => {
-    const input = `
-      1abc2
-      pqr3stu8vwx
-      a1b2c3d4e5f
-      treb7uchet
-    `;
-
     test('solution is 142 for part 1', () => {
+      const input = `
+        1abc2
+        pqr3stu8vwx
+        a1b2c3d4e5f
+        treb7uchet
+      `;
       const calibrationSum = calculateCalibrationSum(input);
       expect(calibrationSum).toEqual(142);
     });
-    test.skip('solution is ? for part 2', () => {
-      throw new Error('Not implemented')
+    test('solution is 281 for part 2', () => {
+      const input = `
+        two1nine
+        eightwothree
+        abcone2threexyz
+        xtwone3four
+        4nineeightseven2
+        zoneight234
+        7pqrstsixteen
+      `;
+      const calibrationSum = calculateCalibrationSum(input, true);
+      expect(calibrationSum).toEqual(281);
     });
   });
   describe('solution is', () => {
@@ -25,12 +34,17 @@ describe('Day 1', () => {
       const calibrationSum = calculateCalibrationSum(input);
       expect(calibrationSum).toEqual(56049);
     });
-    test.skip('? for part 2', () => {
-      const calibrationSum = calculateCalibrationSum(input);
-      expect(calibrationSum).toEqual(142);
+    test('54530 for part 2', () => {
+      const calibrationSum = calculateCalibrationSum(input, true);
+      expect(calibrationSum).toEqual(54530);
     });
   });
 
   // region Tests for smaller parts
+  test('2twone is 21', () => {
+    const input = '2twone';
+    const calibrationSum = calculateCalibrationSum(input, true);
+    expect(calibrationSum).toEqual(21);
+  })
   // endregion
 });
--- util/typescript/string.ts
+++ util/typescript/string.ts
@@ -5,5 +5,9 @@ export function splitLines(text: string, trimText = true, trimLines = true): str
   return text.split('\n').map((l) => trimLines ? l.trim() : l);
 }
 
+export function revertString(text: string): string {
+  return text.split('').reverse().join('');
+}
+
 export const lowerCaseAlphabet = 'abcdefghijklmnopqrstuvwxyz';
 export const upperCaseAlphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
