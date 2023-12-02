18bfad0d7732b78ebfa323a841126a2f7430c4a4
Lennart Hensler <lennart@hensler.email>
2023-12-02 16:12:50 +0100

    Cleanup generate day

--- tool/src/aoctl-generate-day.ts
+++ tool/src/aoctl-generate-day.ts
@@ -5,13 +5,14 @@ import * as Handlebars from 'handlebars';
 import * as path from 'path';
 import * as fs from 'node:fs/promises';
 
+const typescriptDayTemplateSource = path.resolve('../template/typescript/day-{{day}}_{{title}}');
+
 const command = new Command()
   .argument('<output>', 'Path to the output directory')
   .option('-d, --day <number>', 'Number of the day', parseDayNumber, 'today')
   .requiredOption('-t, --title <title>', 'Title of the puzzle')
   .action(generateDayFiles);
-
-const typescriptDayTemplateSource = path.resolve('../template/typescript/day-{{day}}_{{title}}');
+command.parse();
 
 function parseDayNumber(value: string): string {
   if (value === 'today') {
@@ -38,30 +39,31 @@ async function generateDayFiles(output: string, options: {day: string, title: st
   }
   const pathOptions = {...options, day: options.day.padStart(2, '0')};
 
-  // TODO Use inquirer for missing options
-
   const sourceDirectory = typescriptDayTemplateSource;
-  const outputDirectoryTemplate = Handlebars.compile(path.basename(sourceDirectory));
-  output = path.join(path.resolve(output), outputDirectoryTemplate(pathOptions));
+  output = path.join(path.resolve(output), renderTemplate(path.basename(sourceDirectory), pathOptions));
   const outputExists = await fs.access(output, fs.constants.F_OK)
     .then(() => true)
     .catch((error) => error.code === 'ENOENT' ? false : Promise.reject(error));
   if (outputExists) {
     console.log(`Error - Output directory already exists: ${path.relative(process.cwd(), output)}`);
     return;
-  } else {
-    await fs.mkdir(output, { recursive: true });
   }
+  await fs.mkdir(output, { recursive: true });
 
   const templateFiles = await fs.readdir(sourceDirectory);
   for (const templateFile of templateFiles) {
-    const outputFilenameTemplate = Handlebars.compile(templateFile);
-    const outputFilename = outputFilenameTemplate(pathOptions);
-
-    const fileTemplate = Handlebars.compile(await fs.readFile(path.join(sourceDirectory, templateFile), 'utf-8'));
-    await fs.writeFile(path.join(output, outputFilename), fileTemplate(options));
+    const outputFilename = renderTemplate(templateFile, pathOptions);
+    const outputContent = await renderFile(path.join(sourceDirectory, templateFile), options);
+    await fs.writeFile(path.join(output, outputFilename), outputContent);
   }
   console.log(`Files have been generated at ${path.relative(process.cwd(), output)}`);
 }
 
-command.parse();
+async function renderFile<T>(filePath: string, context: T): Promise<string> {
+  return renderTemplate(await fs.readFile(filePath, 'utf-8'), context);
+}
+
+function renderTemplate<T>(template: string, context: T): string {
+  const render = Handlebars.compile<T>(template);
+  return render(context);
+}
