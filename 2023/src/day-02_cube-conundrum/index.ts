6d19fec8625f5f772a8e701d038a577f6c5b29e0
Lennart Hensler <lennart@hensler.email>
2023-12-02 09:31:42 +0100

    2023 Day 2 Part 1

---
+++ 2023/src/day-02_cube-conundrum/index.ts
@@ -0,0 +1,64 @@
+import { splitLines } from '@util';
+
+const colors = ['red', 'green', 'blue'] as const;
+type Color = typeof colors[number];
+
+interface GameConfig {
+  red: number;
+  green: number;
+  blue: number;
+}
+
+interface GameRecord {
+  id: number;
+  cubeSets: Record<Color, number>[];
+}
+
+export function solvePart1(input: string): number {
+  const lines = splitLines(input);
+  const games = lines.map(parseGame);
+  const gameConfig = {
+    red: 12,
+    green: 13,
+    blue: 14,
+  };
+  const possibleGames = games.filter((r) => isGamePossible(r, gameConfig));
+  return possibleGames.reduce((s, r) => s + r.id, 0);
+}
+
+function isGamePossible(record: GameRecord, config: GameConfig): boolean {
+  const maxValues = record.cubeSets.reduce((max, record) => {
+    for (const color of colors) {
+      const colorValue = record[color];
+      const colorMax = max[color];
+      if (colorValue > colorMax) {
+        max[color] = colorValue;
+      }
+    }
+    return max;
+  }, {red: 0, green: 0, blue: 0})
+  for (const color of colors) {
+    const colorMax = maxValues[color];
+    const colorLimit = config[color];
+    if (colorMax > colorLimit) {
+      return false;
+    }
+  }
+  return true;
+}
+
+function parseGame(line: string): GameRecord {
+  const [header, body] = line.split(':');
+  const id = Number(header.split(' ')[1]);
+  if (isNaN(id)) {
+    throw new Error(`Unable to parse id for line: ${line}`);
+  }
+  const cubeSets = body.split(';').map((entry) => {
+    return entry.split(',').reduce((cubeSet, colorValue) => {
+      const [value, color] = colorValue.trim().split(' ');
+      (cubeSet as Record<string, number>)[color] = Number(value);
+      return cubeSet;
+    }, {red: 0, green: 0, blue: 0})
+  });
+  return {id, cubeSets};
+}
