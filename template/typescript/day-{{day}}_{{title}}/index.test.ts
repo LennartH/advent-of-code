9a4b18b7a166432ad9421601bf07555567b6e28d
Lennart Hensler <lennart@hensler.email>
2023-12-02 14:00:16 +0100

    Improve typescript template

--- template/typescript/day-{{day}}_{{title}}/index.test.ts
+++ template/typescript/day-{{day}}_{{title}}/index.test.ts
@@ -1,29 +1,41 @@
 import { readFile } from '@util';
+import { solvePart1, solvePart2 } from './index';
 
 describe('Day {{day}}', () => {
   describe('example input', () => {
     const input = `
     
     `;
+    const part1Solution = null;
+    const part2Solution = null;
 
-    test('solution is ? for part 1', () => {
-      throw new Error('Not implemented')
+    test(`solution is ${part1Solution ?? '?'} for part 1`, () => {
+      const result = solvePart1(input);
+      expect(result).toEqual(part1Solution);
     });
-    test.skip('solution is ? for part 2', () => {
-      throw new Error('Not implemented')
+    test.skip(`solution is ${part2Solution ?? '?'} for part 2`, () => {
+      const result = solvePart2(input);
+      expect(result).toEqual(part2Solution);
     });
   });
+
   describe('solution is', () => {
     const inputPath = `${__dirname}/input`;
     const input = readFile(inputPath);
-    test('? for part 1', () => {
-      throw new Error('Not implemented')
+    const part1Solution = null;
+    const part2Solution = null;
+
+    test(`${part1Solution ?? '?'} for part 1`, () => {
+      const result = solvePart1(input);
+      expect(result).toEqual(part1Solution);
     });
-    test.skip('? for part 2', () => {
-      throw new Error('Not implemented')
+    test.skip(`${part2Solution ?? '?'} for part 2`, () => {
+      const result = solvePart2(input);
+      expect(result).toEqual(part2Solution);
     });
   });
 
-  // region Tests for smaller parts
+  // region Function Specific Tests
+
   // endregion
 });
--- template/typescript/day-{{day}}_{{title}}/index.ts
+++ template/typescript/day-{{day}}_{{title}}/index.ts
@@ -1,1 +1,21 @@
 import { splitLines } from '@util';
+
+// region Types and Globals
+
+// endregion
+
+export function solvePart1(input: string): number {
+  const lines = splitLines(input);
+  // TODO Implement solution
+  return Number.NaN;
+}
+
+export function solvePart2(input: string): number {
+  const lines = splitLines(input);
+  // TODO Implement solution
+  return Number.NaN;
+}
+
+// region Shared Code
+
+// endregion
