385ec257dbf0b9dd8608749404dc61e0a4298044
Lennart Hensler <lennart@hensler.email>
2023-12-04 07:33:00 +0100

    2023 Day 4 Part 1

---
+++ 2023/src/day-04_scratchcards/index.test.ts
@@ -0,0 +1,46 @@
+import { readFile } from '@util';
+import { solvePart1, solvePart2 } from './index';
+
+describe('Day 4', () => {
+  describe('example input', () => {
+    const input = `
+      Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+      Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+      Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+      Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+      Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+      Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11
+    `;
+    const part1Solution = 13;
+    const part2Solution = null;
+
+    test(`solution is ${part1Solution ?? '?'} for part 1`, () => {
+      const result = solvePart1(input);
+      expect(result).toEqual(part1Solution);
+    });
+    test.skip(`solution is ${part2Solution ?? '?'} for part 2`, () => {
+      const result = solvePart2(input);
+      expect(result).toEqual(part2Solution);
+    });
+  });
+
+  describe('solution is', () => {
+    const inputPath = `${__dirname}/input`;
+    const input = readFile(inputPath);
+    const part1Solution = 21105;
+    const part2Solution = null;
+
+    test(`${part1Solution ?? '?'} for part 1`, () => {
+      const result = solvePart1(input);
+      expect(result).toEqual(part1Solution);
+    });
+    test.skip(`${part2Solution ?? '?'} for part 2`, () => {
+      const result = solvePart2(input);
+      expect(result).toEqual(part2Solution);
+    });
+  });
+
+  // region Function Specific Tests
+
+  // endregion
+});
