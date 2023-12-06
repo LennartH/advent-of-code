82632889802716aea88338e0ff01c68b7134ea9d
Lennart Hensler <lennart@hensler.email>
2023-12-06 21:47:08 +0100

    template: typescript - Minor adjustment to test structure

--- template/typescript/day-{{day}}_{{title}}/index.test.ts
+++ template/typescript/day-{{day}}_{{title}}/index.test.ts
@@ -2,7 +2,7 @@ import { readFile } from '@util';
 import { solvePart1, solvePart2 } from './index';
 
 describe('Day {{day}}: {{title}}', () => {
-  describe('example input', () => {
+  describe('Example input', () => {
     const input = `
     
     `;
@@ -19,23 +19,25 @@ describe('Day {{day}}: {{title}}', () => {
     });
   });
 
-  describe('solution is', () => {
+  describe('Real input', () => {
     const inputPath = `${__dirname}/input`;
     const input = readFile(inputPath);
     const part1Solution = null;
     const part2Solution = null;
 
-    test(`${part1Solution ?? '?'} for part 1`, () => {
+    test(`solution is ${part1Solution ?? '?'} for part 1`, () => {
       const result = solvePart1(input);
       expect(result).toEqual(part1Solution);
     });
-    test.skip(`${part2Solution ?? '?'} for part 2`, () => {
+    test.skip(`solution is ${part2Solution ?? '?'} for part 2`, () => {
       const result = solvePart2(input);
       expect(result).toEqual(part2Solution);
     });
   });
 
   // region Function Specific Tests
-
+  describe.skip('Make sure that', () => {
+    // Add tests if all hope is lost (it's okay to cry)
+  });
   // endregion
 });
