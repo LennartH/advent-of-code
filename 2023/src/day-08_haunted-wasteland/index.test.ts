336141df520d53aeef6cc5332f3fed7510cb9521
Lennart Hensler <lennart@hensler.email>
2023-12-08 20:20:53 +0100

    2023 Day 8 Part 2

--- 2023/src/day-08_haunted-wasteland/index.test.ts
+++ 2023/src/day-08_haunted-wasteland/index.test.ts
@@ -3,7 +3,7 @@ import { solvePart1, solvePart2 } from './index';
 
 describe('Day 8: Haunted Wasteland', () => {
   describe('Example input', () => {
-    const input = `
+    const part1Input = `
       LLR
 
       AAA = (BBB, BBB)
@@ -11,14 +11,27 @@ describe('Day 8: Haunted Wasteland', () => {
       ZZZ = (ZZZ, ZZZ)
     `;
     const part1Solution = 6;
-    const part2Solution = null;
+
+    const part2Input = `
+      LR
+      
+      11A = (11B, XXX)
+      11B = (XXX, 11Z)
+      11Z = (11B, XXX)
+      22A = (22B, XXX)
+      22B = (22C, 22C)
+      22C = (22Z, 22Z)
+      22Z = (22B, 22B)
+      XXX = (XXX, XXX)
+    `;
+    const part2Solution = 6;
 
     test(`solution is ${part1Solution ?? '?'} for part 1`, () => {
-      const result = solvePart1(input);
+      const result = solvePart1(part1Input);
       expect(result).toEqual(part1Solution);
     });
-    test.skip(`solution is ${part2Solution ?? '?'} for part 2`, () => {
-      const result = solvePart2(input);
+    test(`solution is ${part2Solution ?? '?'} for part 2`, () => {
+      const result = solvePart2(part2Input);
       expect(result).toEqual(part2Solution);
     });
   });
@@ -27,13 +40,13 @@ describe('Day 8: Haunted Wasteland', () => {
     const inputPath = `${__dirname}/input`;
     const input = readFile(inputPath);
     const part1Solution = 21883;
-    const part2Solution = null;
+    const part2Solution = 12833235391111;
 
     test(`solution is ${part1Solution ?? '?'} for part 1`, () => {
       const result = solvePart1(input);
       expect(result).toEqual(part1Solution);
     });
-    test.skip(`solution is ${part2Solution ?? '?'} for part 2`, () => {
+    test(`solution is ${part2Solution ?? '?'} for part 2`, () => {
       const result = solvePart2(input);
       expect(result).toEqual(part2Solution);
     });
--- 2023/src/day-08_haunted-wasteland/index.ts
+++ 2023/src/day-08_haunted-wasteland/index.ts
@@ -1,4 +1,4 @@
-import { splitLines } from '@util';
+import { splitLines, leastCommonMultiple } from '@util';
 
 // region Types and Globals
 interface Map {
@@ -8,12 +8,37 @@ interface Map {
 // endregion
 
 export function solvePart1(input: string): number {
-  const {instructions, network} = parseMap(splitLines(input));
+  const map = parseMap(splitLines(input));
+  return pathLength('AAA', map, (node) => node !== 'ZZZ');
+}
 
+export function solvePart2(input: string): number {
+  const map = parseMap(splitLines(input));
+
+  const nodes = Object.keys(map.network).filter((n) => n.endsWith('A'));
+  const pathLengths = nodes.map((n) => pathLength(n, map, (node) => !node.endsWith('Z')));
+  return leastCommonMultiple(pathLengths);
+}
+
+// region Shared Code
+function parseMap(lines: string[]): Map {
+  const instructions = lines[0].split('') as ('L' | 'R')[];
+  const network = lines.splice(2).reduce((network, line) => {
+    const matches = line.match(/[0-9A-Z]+/g);
+    if (matches == null || matches.length < 3) {
+      throw new Error(`Unable to parse line: ${line}`);
+    }
+    network[matches[0]] = {L: matches[1], R: matches[2]}
+    return network;
+  }, {} as Record<string, Record<'L' | 'R', string>>);
+  return { instructions, network }
+}
+
+function pathLength(start: string, {instructions, network}: Map, predicate: (n: string) => boolean): number {
   let counter = 0;
   let instructionIndex = 0;
-  let node = 'AAA';
-  while (node !== 'ZZZ') {
+  let node = start;
+  while (predicate(node)) {
     const instruction = instructions[instructionIndex];
     node = network[node][instruction];
     instructionIndex++;
@@ -22,27 +47,6 @@ export function solvePart1(input: string): number {
     }
     counter++;
   }
-
   return counter;
-}
-
-export function solvePart2(input: string): number {
-  const lines = splitLines(input);
-  // TODO Implement solution
-  return Number.NaN;
-}
-
-// region Shared Code
-function parseMap(lines: string[]): Map {
-  const instructions = lines[0].split('') as ('L' | 'R')[];
-  const network = lines.splice(2).reduce((network, line) => {
-    const matches = line.match(/[A-Z]+/g);
-    if (matches == null || matches.length < 3) {
-      throw new Error(`Unable to parse line: ${line}`);
-    }
-    network[matches[0]] = {L: matches[1], R: matches[2]}
-    return network;
-  }, {} as Record<string, Record<'L' | 'R', string>>);
-  return { instructions, network }
 }
 // endregion
