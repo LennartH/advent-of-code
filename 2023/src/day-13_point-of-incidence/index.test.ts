717cd114619025d5f66289af7d8c4a604ea2ce67
Lennart Hensler <lennart@hensler.email>
2023-12-13 09:49:31 +0100

    2023 Day 13 Part 2

--- 2023/src/day-13_point-of-incidence/index.test.ts
+++ 2023/src/day-13_point-of-incidence/index.test.ts
@@ -21,13 +21,13 @@ describe('Day 13: Point Of Incidence', () => {
       #....#..#
     `;
     const part1Solution = 405;
-    const part2Solution = null;
+    const part2Solution = 400;
 
     test(`solution is ${part1Solution ?? '?'} for part 1`, () => {
       const result = solvePart1(input);
       expect(result).toEqual(part1Solution);
     });
-    test.skip(`solution is ${part2Solution ?? '?'} for part 2`, () => {
+    test(`solution is ${part2Solution ?? '?'} for part 2`, () => {
       const result = solvePart2(input);
       expect(result).toEqual(part2Solution);
     });
@@ -37,21 +37,15 @@ describe('Day 13: Point Of Incidence', () => {
     const inputPath = `${__dirname}/input`;
     const input = readFile(inputPath);
     const part1Solution = 29130;
-    const part2Solution = null;
+    const part2Solution = 33438;
 
     test(`solution is ${part1Solution ?? '?'} for part 1`, () => {
       const result = solvePart1(input);
       expect(result).toEqual(part1Solution);
     });
-    test.skip(`solution is ${part2Solution ?? '?'} for part 2`, () => {
+    test(`solution is ${part2Solution ?? '?'} for part 2`, () => {
       const result = solvePart2(input);
       expect(result).toEqual(part2Solution);
     });
   });
-
-  // region Function Specific Tests
-  describe.skip('Make sure that', () => {
-    // Add tests if all hope is lost (it's okay to cry)
-  });
-  // endregion
 });
--- 2023/src/day-13_point-of-incidence/index.ts
+++ 2023/src/day-13_point-of-incidence/index.ts
@@ -1,10 +1,5 @@
-import { formatGrid, splitLines, sum } from '@util';
+import { formatGrid, sum } from '@util';
 import { ArrayGrid, Grid } from '@util/grid';
-import { pipe } from 'iter-ops';
-
-// region Types and Globals
-
-// endregion
 
 export function solvePart1(input: string): number {
   const grids = input.split(/\n\s*\n/).map((p) => ArrayGrid.fromInput(p));
@@ -12,22 +7,17 @@ export function solvePart1(input: string): number {
 }
 
 export function solvePart2(input: string): number {
-  const lines = splitLines(input);
-  // TODO Implement solution
-  return Number.NaN;
+  const grids = input.split(/\n\s*\n/).map((p) => ArrayGrid.fromInput(p));
+  return grids.map(findFoldLineIdWhenFixingSmudge).reduce(sum);
 }
 
 // region Shared Code
 function findFoldLineId(grid: Grid<string>): number {
   for (let x = 0; x < grid.width - 1; x++) {
-    const columnA = [...grid.column(x)].map(({value}) => value).join('');
-    const columnB = [...grid.column(x + 1)].map(({value}) => value).join('');
-    if (columnA === columnB) {
+    if (columnsAreEqual(grid, x, x + 1)) {
       let foundFoldLineIndex = true;
       for (let x1 = x - 1, x2 = x + 2; x1 >= 0 && x2 < grid.width; x1--, x2++) {
-        const columnA = [...grid.column(x1)].map(({value}) => value).join('');
-        const columnB = [...grid.column(x2)].map(({value}) => value).join('');
-        if (columnA !== columnB) {
+        if (!columnsAreEqual(grid, x1, x2)) {
           foundFoldLineIndex = false;
           break;
         }
@@ -39,14 +29,10 @@ function findFoldLineId(grid: Grid<string>): number {
   }
 
   for (let y = 0; y < grid.height - 1; y++) {
-    const rowA = [...grid.row(y)].map(({value}) => value).join('');
-    const rowB = [...grid.row(y + 1)].map(({value}) => value).join('');
-    if (rowA === rowB) {
+    if (rowsAreEqual(grid, y, y + 1)) {
       let foundFoldLineIndex = true;
       for (let y1 = y - 1, y2 = y + 2; y1 >= 0 && y2 < grid.height; y1--, y2++) {
-        const rowA = [...grid.row(y1)].map(({value}) => value).join('');
-        const rowB = [...grid.row(y2)].map(({value}) => value).join('');
-        if (rowA !== rowB) {
+        if (!rowsAreEqual(grid, y1, y2)) {
           foundFoldLineIndex = false;
           break;
         }
@@ -58,5 +44,89 @@ function findFoldLineId(grid: Grid<string>): number {
   }
 
   throw new Error(`Unable to find fold line for grid:\n${formatGrid(grid)}`)
+}
+
+function findFoldLineIdWhenFixingSmudge(grid: Grid<string>): number {
+  for (let x = 0; x < grid.width - 1; x++) {
+    const startingColumnsAreEqual = columnsAreEqual(grid, x, x + 1);
+    const startingColumnsAreOffByOne = columnsAreOffByOne(grid, x, x+1);
+    if (startingColumnsAreEqual || startingColumnsAreOffByOne) {
+      let fixedSmudge = startingColumnsAreOffByOne;
+      let foundFoldLineIndex = true;
+      for (let x1 = x - 1, x2 = x + 2; x1 >= 0 && x2 < grid.width; x1--, x2++) {
+        if (!columnsAreEqual(grid, x1, x2)) {
+          if (!fixedSmudge && columnsAreOffByOne(grid, x1, x2)) {
+            fixedSmudge = true;
+            continue;
+          }
+          foundFoldLineIndex = false;
+          break;
+        }
+      }
+      if (foundFoldLineIndex && fixedSmudge) {
+        return x + 1;
+      }
+    }
+  }
+
+  for (let y = 0; y < grid.height - 1; y++) {
+    const startingRowsAreEqual = rowsAreEqual(grid, y, y + 1);
+    const startingRowsAreOffByOne = rowsAreOffByOne(grid, y, y + 1);
+    if (startingRowsAreEqual || startingRowsAreOffByOne) {
+      let fixedSmudge = startingRowsAreOffByOne;
+      let foundFoldLineIndex = true;
+      for (let y1 = y - 1, y2 = y + 2; y1 >= 0 && y2 < grid.height; y1--, y2++) {
+        if (!rowsAreEqual(grid, y1, y2)) {
+          if (!fixedSmudge && rowsAreOffByOne(grid, y1, y2)) {
+            fixedSmudge = true;
+            continue;
+          }
+          foundFoldLineIndex = false;
+          break;
+        }
+      }
+      if (foundFoldLineIndex && fixedSmudge) {
+        return 100 * (y + 1);
+      }
+    }
+  }
+
+  throw new Error(`Unable to find fold line for grid:\n${formatGrid(grid)}`)
+}
+
+function columnsAreOffByOne(grid: Grid<string>, x1: number, x2: number): boolean {
+  const columnA = [...grid.column(x1)].map(({value}) => value).join('');
+  const columnB = [...grid.column(x2)].map(({value}) => value).join('');
+  let mismatchCount = 0;
+  for (let i = 0; i < columnA.length; i++) {
+    if (columnA[i] !== columnB[i]) {
+      mismatchCount++;
+    }
+  }
+  return mismatchCount === 1;
+}
+
+function rowsAreOffByOne(grid: Grid<string>, y1: number, y2: number): boolean {
+  const rowA = [...grid.row(y1)].map(({value}) => value).join('');
+  const rowB = [...grid.row(y2)].map(({value}) => value).join('');
+  let mismatchCount = 0;
+  for (let i = 0; i < rowA.length; i++) {
+    if (rowA[i] !== rowB[i]) {
+      mismatchCount++;
+    }
+  }
+  return mismatchCount === 1;
+}
+
+function columnsAreEqual(grid: Grid<string>, x1: number, x2: number): boolean {
+  const columnA = [...grid.column(x1)].map(({value}) => value).join('');
+  const columnB = [...grid.column(x2)].map(({value}) => value).join('');
+  return columnA === columnB;
+}
+
+function rowsAreEqual(grid: Grid<string>, y1: number, y2: number): boolean {
+  const rowA = [...grid.row(y1)].map(({value}) => value).join('');
+  const rowB = [...grid.row(y2)].map(({value}) => value).join('');
+  return rowA === rowB;
 }
 // endregion
