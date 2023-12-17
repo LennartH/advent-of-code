04adb400bd1a9803c06e28b5fbd62d3696f64f83
Lennart Hensler <lennart@hensler.email>
2023-12-17 13:23:20 +0100

    2023 Day 17 Part 2 (~50 min runtime)

--- 2023/src/day-17_clumsy-crucible/index.ts
+++ 2023/src/day-17_clumsy-crucible/index.ts
@@ -1,28 +1,26 @@
 import {
-  ArrayGrid, formatGrid,
+  ArrayGrid,
+  CardinalDirection2D,
+  directionFromName,
+  formatGrid,
   getDirections,
   Grid,
-  manhattanDistance, oppositeOf,
+  manhattanDistance,
+  oppositeOf,
   PlainPoint,
   pointsEqual,
   pointToString,
-  splitLines,
   StraightCardinalDirectionName
 } from '@util';
 
-// region Types and Globals
-
-// endregion
-
 export function solvePart1(input: string): number {
   const grid = ArrayGrid.fromInput(input, Number);
-  return findHeatLossMinimum(grid, 3);
+  return findHeatLossMinimum(grid, 0, 3);
 }
 
 export function solvePart2(input: string): number {
-  const lines = splitLines(input);
-  // TODO Implement solution
-  return Number.NaN;
+  const grid = ArrayGrid.fromInput(input, Number);
+  return findHeatLossMinimum(grid, 4, 10);
 }
 
 // region Shared Code
@@ -34,66 +32,76 @@ interface VisitedNode {
   readonly direction: StraightCardinalDirectionName;
   readonly predecessor?: VisitedNode;
 }
-function findHeatLossMinimum(grid: Grid<number>, maxStraightMoves: number): number {
+function findHeatLossMinimum(grid: Grid<number>, minStraightMoves: number, maxStraightMoves: number): number {
   const nodeKey = (node: VisitedNode) => `${pointToString(node.position)}|${node.direction},${node.straightCount}`;
-  const calculateDistance = (from: PlainPoint, to: PlainPoint) => manhattanDistance(from, to);
 
   const start = {x: 0, y: 0};
   const target = {x: grid.width - 1, y: grid.height - 1};
   const visited = new Set<string>();
-  const openNodes: VisitedNode[] = [{
-    position: start,
-    cost: 0,
-    distance: calculateDistance(start, target),
-    straightCount: 0,
-    direction: 'E',
-  }];
+  const openNodes: VisitedNode[] = [
+    {
+      position: {x: 1, y: 0},
+      cost: grid.get(1, 0),
+      distance: manhattanDistance(1, 0, target),
+      straightCount: 1,
+      direction: 'E',
+    },
+    {
+      position: {x: 0, y: 1},
+      cost: grid.get(0, 1),
+      distance: manhattanDistance(0, 1, target),
+      straightCount: 1,
+      direction: 'S',
+    }
+  ];
 
-  let debugStep = 0;
+  let debugStep = 1;
   const possibleDirections = getDirections('cardinal', {withDiagonals: false});
   let finalNode: VisitedNode | null = null;
   do {
     const node = openNodes.pop()!;
     if (debugStep % 1000 === 0) {
       console.log(
-        `Step ${debugStep}`.padStart(20, ' '),
+        `Step ${debugStep}`.padStart(20, ' '), `(${openNodes.length.toString().padStart(4, ' ')} open nodes)`,
         '- Current best:', `(${node.position.x.toString().padStart(3, ' ')}, ${node.position.y.toString().padStart(3, ' ')})`,
         '| cost:', node.cost.toString().padStart(4, ' '),
         'distance:', node.distance.toString().padStart(3, ' ')
       );
     }
     if (pointsEqual(node.position, target)) {
+      if (node.straightCount < minStraightMoves) {
+        continue
+      }
       console.log(
-        `Step ${debugStep}`.padStart(20, ' '),
+        `Step ${debugStep}`.padStart(20, ' '), `(${openNodes.length.toString().padStart(4, ' ')} open nodes)`,
         '-     Solution:', `(${node.position.x.toString().padStart(3, ' ')}, ${node.position.y.toString().padStart(3, ' ')})`,
         '| cost:', node.cost.toString().padStart(4, ' '),
         'distance:', node.distance.toString().padStart(3, ' ')
       );
       finalNode = node;
       break;
-      // if (finalNode == null || node.cost < finalNode.cost) {
-      //   finalNode = node;
-      // }
-      // continue;
     }
     visited.add(nodeKey(node));
 
-    const directions = node.straightCount < maxStraightMoves ?
-      possibleDirections.filter(({name}) => name !== oppositeOf(node.direction).name) :
-      possibleDirections.filter(({name}) => name !== node.direction && name !== oppositeOf(node.direction).name);
+    let directions: CardinalDirection2D[] = possibleDirections.filter(({name}) => name !== oppositeOf(node.direction).name);
+    if (node.straightCount < minStraightMoves) {
+      directions = [directionFromName(node.direction)]
+    } else if (node.straightCount >= maxStraightMoves) {
+      directions = possibleDirections.filter(({name}) => name !== node.direction && name !== oppositeOf(node.direction).name);
+    }
+
     for (const {position: nextPosition, value, direction} of grid.adjacentFrom(node.position, {directions})) {
       const nextStraightCount = direction.name === node.direction ? node.straightCount + 1 : 1;
       const nextNode: VisitedNode = {
         position: nextPosition,
         cost: node.cost + value,
-        distance: calculateDistance(nextPosition, target),
+        distance: manhattanDistance(nextPosition, target),
         straightCount: nextStraightCount,
         direction: direction.name as StraightCardinalDirectionName,
         predecessor: node,
       };
       if (visited.has(nodeKey(nextNode))) {
         continue;
-
       }
 
       const index = openNodes.findIndex((node) => nodeKey(node) === nodeKey(nextNode));
@@ -102,6 +110,8 @@ function findHeatLossMinimum(grid: Grid<number>, maxStraightMoves: number): numb
       } else if (nextNode.cost < openNodes[index].cost) {
         openNodes[index] = nextNode;
       }
+      // TODO Limit size of open nodes to 100?
+      // TODO Use SplitList or Binary/Fibonacci Heap
     }
 
     openNodes.sort((n1, n2) => (n2.cost + n2.distance) - (n1.cost + n1.distance));
