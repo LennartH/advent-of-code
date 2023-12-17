69de62a0f565656dc8d843e12d3d6acb3b88f225
Lennart Hensler <lennart@hensler.email>
2023-12-17 13:23:20 +0100

    2023 Day 17 Part 2 (~50 min runtime)

--- 2023/src/day-17_clumsy-crucible/index.test.ts
+++ 2023/src/day-17_clumsy-crucible/index.test.ts
@@ -19,15 +19,28 @@ describe('Day 17: Clumsy Crucible', () => {
       4322674655533
     `;
     const part1Solution = 102;
-    const part2Solution = null;
+    const part2Solution = 94;
+
+    const additionalPart2Input = `
+      111111111111
+      999999999991
+      999999999991
+      999999999991
+      999999999991
+    `;
+    const additionalPart2Solution = 71;
 
     test(`solution is ${part1Solution ?? '?'} for part 1`, () => {
       const result = solvePart1(input);
       expect(result).toEqual(part1Solution);
     });
-    test.skip(`solution is ${part2Solution ?? '?'} for part 2`, () => {
+    test(`solution is ${part2Solution ?? '?'} for part 2`, () => {
       const result = solvePart2(input);
       expect(result).toEqual(part2Solution);
+    });
+    test(`additional solution is ${additionalPart2Solution ?? '?'} for part 2`, () => {
+      const result = solvePart2(additionalPart2Input);
+      expect(result).toEqual(additionalPart2Solution);
     });
   });
 
@@ -35,21 +48,15 @@ describe('Day 17: Clumsy Crucible', () => {
     const inputPath = `${__dirname}/input`;
     const input = readFile(inputPath);
     const part1Solution = 1065;
-    const part2Solution = null;
+    const part2Solution = 1249;
 
     test(`solution is ${part1Solution ?? '?'} for part 1`, () => {
       const result = solvePart1(input);
       expect(result).toEqual(part1Solution);
     });
-    test.skip(`solution is ${part2Solution ?? '?'} for part 2`, () => {
+    test(`solution is ${part2Solution ?? '?'} for part 2`, () => {
       const result = solvePart2(input);
       expect(result).toEqual(part2Solution);
     });
   });
-
-  // region Function Specific Tests
-  describe.skip('Make sure that', () => {
-    // Add tests if all hope is lost (it's okay to cry)
-  });
-  // endregion
 });
