abecbc81cae56926faef54b7e9c5b1788fd90435
Lennart Hensler <lennart@hensler.email>
2023-12-18 09:35:21 +0100

    2023 Day 18 Part 1

---
+++ 2023/src/day-18_lavaduct-lagoon/index.test.ts
@@ -0,0 +1,56 @@
+import { readFile } from '@util';
+import { solvePart1, solvePart2 } from './index';
+
+describe('Day 18: Lavaduct Lagoon', () => {
+  describe('Example input', () => {
+    const input = `
+      R 6 (#70c710)
+      D 5 (#0dc571)
+      L 2 (#5713f0)
+      D 2 (#d2c081)
+      R 2 (#59c680)
+      D 2 (#411b91)
+      L 5 (#8ceee2)
+      U 2 (#caa173)
+      L 1 (#1b58a2)
+      U 2 (#caa171)
+      R 2 (#7807d2)
+      U 3 (#a77fa3)
+      L 2 (#015232)
+      U 2 (#7a21e3)
+    `;
+    const part1Solution = 62;
+    const part2Solution = null;
+
+    test(`solution is ${part1Solution ?? '?'} for part 1`, () => {
+      const result = solvePart1(input);
+      expect(result).toEqual(part1Solution);
+    });
+    test.skip(`solution is ${part2Solution ?? '?'} for part 2`, () => {
+      const result = solvePart2(input);
+      expect(result).toEqual(part2Solution);
+    });
+  });
+
+  describe('Real input', () => {
+    const inputPath = `${__dirname}/input`;
+    const input = readFile(inputPath);
+    const part1Solution = 50603;
+    const part2Solution = null;
+
+    test(`solution is ${part1Solution ?? '?'} for part 1`, () => {
+      const result = solvePart1(input);
+      expect(result).toEqual(part1Solution);
+    });
+    test.skip(`solution is ${part2Solution ?? '?'} for part 2`, () => {
+      const result = solvePart2(input);
+      expect(result).toEqual(part2Solution);
+    });
+  });
+
+  // region Function Specific Tests
+  describe.skip('Make sure that', () => {
+    // Add tests if all hope is lost (it's okay to cry)
+  });
+  // endregion
+});
