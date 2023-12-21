6cd7f8ead713576201deb28f5e00ccee85aef248
Lennart Hensler <lennart@hensler.email>
2023-12-21 23:10:05 +0100

    2023 Day 21 Part 2: the struggle gets real

--- 2023/src/day-21_step-counter/index.test.ts
+++ 2023/src/day-21_step-counter/index.test.ts
@@ -17,15 +17,21 @@ describe('Day 21: Step Counter', () => {
       ...........
     `;
     const part1Solution = 16;
-    const part2Solution = null;
 
     test(`solution is ${part1Solution ?? '?'} for part 1`, () => {
       const result = solvePart1(input, 6);
       expect(result).toEqual(part1Solution);
     });
-    test.skip(`solution is ${part2Solution ?? '?'} for part 2`, () => {
-      const result = solvePart2(input);
-      expect(result).toEqual(part2Solution);
+    test.each([
+      // [10, 50],
+      // [50, 1594],
+      // [100, 6536],
+      // [500, 167004],
+      // [1000, 668697],
+      [5000, 16733044],
+    ])(`part 2 solution for %s steps is %s`, (numberOfSteps, solution) => {
+      const result = solvePart2(input, numberOfSteps);
+      expect(result).toEqual(solution);
     });
   });
 
@@ -39,8 +45,8 @@ describe('Day 21: Step Counter', () => {
       const result = solvePart1(input, 64);
       expect(result).toEqual(part1Solution);
     });
-    test.skip(`solution is ${part2Solution ?? '?'} for part 2`, () => {
-      const result = solvePart2(input);
+    test(`solution is ${part2Solution ?? '?'} for part 2`, () => {
+      const result = solvePart2(input, 26501365);
       expect(result).toEqual(part2Solution);
     });
   });
