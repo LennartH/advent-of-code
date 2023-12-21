6590bd7f60fd88f7320d797cc4e12209defcfa81
Lennart Hensler <lennart@hensler.email>
2023-12-21 09:24:58 +0100

    2023 Day 21 Part 1

--- 2023/src/day-21_step-counter/index.test.ts
+++ 2023/src/day-21_step-counter/index.test.ts
@@ -1,16 +1,26 @@
 import { readFile } from '@util';
 import { solvePart1, solvePart2 } from './index';
 
-describe('Day {{day}}: {{title}}', () => {
+describe('Day 21: Step Counter', () => {
   describe('Example input', () => {
     const input = `
-
+      ...........
+      .....###.#.
+      .###.##..#.
+      ..#.#...#..
+      ....#.#....
+      .##..S####.
+      .##..#...#.
+      .......##..
+      .##.#.####.
+      .##..##.##.
+      ...........
     `;
-    const part1Solution = null;
+    const part1Solution = 16;
     const part2Solution = null;
 
     test(`solution is ${part1Solution ?? '?'} for part 1`, () => {
-      const result = solvePart1(input);
+      const result = solvePart1(input, 6);
       expect(result).toEqual(part1Solution);
     });
     test.skip(`solution is ${part2Solution ?? '?'} for part 2`, () => {
@@ -22,11 +32,11 @@ describe('Day {{day}}: {{title}}', () => {
   describe('Real input', () => {
     const inputPath = `${__dirname}/input`;
     const input = readFile(inputPath);
-    const part1Solution = null;
+    const part1Solution = 3572;
     const part2Solution = null;
 
     test(`solution is ${part1Solution ?? '?'} for part 1`, () => {
-      const result = solvePart1(input);
+      const result = solvePart1(input, 64);
       expect(result).toEqual(part1Solution);
     });
     test.skip(`solution is ${part2Solution ?? '?'} for part 2`, () => {
