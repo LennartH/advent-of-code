e0fc8789c20d438b6208271f8373309a5db7df3b
Lennart Hensler <lennart@hensler.email>
2023-12-26 13:21:53 +0100

    2023 Day 22 Part 2

--- 2023/src/day-22_sand-slabs/index.test.ts
+++ 2023/src/day-22_sand-slabs/index.test.ts
@@ -13,13 +13,13 @@ describe('Day 22: Sand Slabs', () => {
       1,1,8~1,1,9
     `;
     const part1Solution = 5;
-    const part2Solution = null;
+    const part2Solution = 7;
 
     test(`solution is ${part1Solution ?? '?'} for part 1`, () => {
       const result = solvePart1(input);
       expect(result).toEqual(part1Solution);
     });
-    test.skip(`solution is ${part2Solution ?? '?'} for part 2`, () => {
+    test(`solution is ${part2Solution ?? '?'} for part 2`, () => {
       const result = solvePart2(input);
       expect(result).toEqual(part2Solution);
     });
@@ -29,21 +29,15 @@ describe('Day 22: Sand Slabs', () => {
     const inputPath = `${__dirname}/input`;
     const input = readFile(inputPath);
     const part1Solution = 524;
-    const part2Solution = null;
+    const part2Solution = 77070;
 
     test(`solution is ${part1Solution ?? '?'} for part 1`, () => {
       const result = solvePart1(input);
       expect(result).toEqual(part1Solution);
     });
-    test.skip(`solution is ${part2Solution ?? '?'} for part 2`, () => {
+    test(`solution is ${part2Solution ?? '?'} for part 2`, () => {
       const result = solvePart2(input);
       expect(result).toEqual(part2Solution);
     });
   });
-
-  // region Function Specific Tests
-  describe.skip('Make sure that', () => {
-    // Add tests if all hope is lost (it's okay to cry)
-  });
-  // endregion
 });
