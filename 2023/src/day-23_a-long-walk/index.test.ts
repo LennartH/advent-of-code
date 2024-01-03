d64b5398738722a6056a4339e7cec3442e5a1816
Lennart Hensler <lennart@hensler.email>
2024-01-03 17:08:05 +0100

    2023 Day 23 Part 2: still slow, but works (except for part 1 example input...)

--- 2023/src/day-23_a-long-walk/index.test.ts
+++ 2023/src/day-23_a-long-walk/index.test.ts
@@ -1,5 +1,5 @@
 import { readFile } from '@util';
-import { solvePart1, solvePart2 } from './index';
+import { gridToGraph, solvePart1, solvePart2 } from './index';
 
 describe('Day 23: A Long Walk', () => {
   describe('Example input', () => {
@@ -67,7 +67,7 @@ describe('Day 23: A Long Walk', () => {
     const inputPath = `${__dirname}/input`;
     const input = readFile(inputPath);
     const part1Solution = 2310;
-    const part2Solution = null;
+    const part2Solution = 6738;
 
     test(`solution is ${part1Solution ?? '?'} for part 1`, () => {
       const result = solvePart1(input);
@@ -80,8 +80,46 @@ describe('Day 23: A Long Walk', () => {
   });
 
   // region Function Specific Tests
-  describe.skip('Make sure that', () => {
-    // Add tests if all hope is lost (it's okay to cry)
+  describe('Make sure that', () => {
+    test('graph from input works', () => {
+      const input = `
+        #.#########
+        #.#.......#
+        #.#.#####.#
+        #..x.....x#
+        ###.#####.#
+        ###......x#
+        #########.#
+      `;
+      // '1,0','3,3','9,3','9,5','9,6'
+      const expectedEdges = new Set([
+        '1,0--3,3 = 5',
+        '3,3--1,0 = 5',
+
+        // Only longest edge survives
+        // '3,3--9,3 = 6',
+        // '9,3--3,3 = 6',
+        '3,3--9,3 = 10',
+        '9,3--3,3 = 10',
+        '3,3--9,5 = 8',
+        '9,5--3,3 = 8',
+
+        '9,3--9,5 = 2',
+        '9,5--9,3 = 2',
+
+        '9,5--9,6 = 1',
+        '9,6--9,5 = 1',
+      ]);
+
+      const graph = gridToGraph(input.replaceAll('x', '.'));
+      const edges = new Set<string>();
+      for (const [from, toNodes] of graph.edges.entries()) {
+        for (const [to, distance] of toNodes.entries()) {
+          edges.add(`${from}--${to} = ${distance}`);
+        }
+      }
+      expect(edges).toEqual(expectedEdges);
+    })
   });
   // endregion
 });
