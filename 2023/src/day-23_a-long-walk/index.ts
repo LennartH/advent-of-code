33274b0452484b44cbe25964b945bfeffd4361c1
Lennart Hensler <lennart@hensler.email>
2024-01-03 14:13:09 +0100

    2023 Day 23 Part 2: slow and not working for real input

--- 2023/src/day-23_a-long-walk/index.ts
+++ 2023/src/day-23_a-long-walk/index.ts
@@ -58,10 +58,13 @@ interface VisitedNode {
   position: PlainPoint;
   distance: number;
   visited: Set<string>;
+
+  previous?: VisitedNode;
+  finalNode?: VisitedNode;
 }
 
 function longestPath(from: PlainPoint, to: PlainPoint, grid: Grid<string>): number {
-  // const distances = new Map<string, number>();
+  const pathNodes = new Map<string, VisitedNode>();
   const open: VisitedNode[] = [{position: from, distance: 0, visited: new Set()}];
 
   let maxLength = 0;
@@ -69,29 +72,64 @@ function longestPath(from: PlainPoint, to: PlainPoint, grid: Grid<string>): numb
     const current = open.pop()!;
     const visited = current.visited;
     const key = pointToString(current.position);
-    if (visited.has(key)) { // || (distances.get(key) || -1) >= current.distance) {
-      continue;
+    if (visited.has(key)) {
+      throw new Error('Moo')
     }
+
     if (pointsEqual(current.position, to)) {
       maxLength = Math.max(current.distance, maxLength);
+      let node = current;
+      while (node.previous != null) {
+        node = node.previous;
+        node.finalNode = current;
+        pathNodes.set(pointToString(node.position), node);
+      }
       continue;
     }
+    if (pathNodes.has(key)) {
+      const pathNode = pathNodes.get(key)!;
+      const delta = current.distance - pathNode.distance;
+      if (delta > 0) {
+        const finalNode = pathNode.finalNode;
+        if (finalNode == null) {
+          throw new Error('Mawp')
+        }
+        let node = current;
+        while (node.previous != null) {
+          node.finalNode = finalNode;
+          pathNodes.set(pointToString(node.position), node);
+          node = node.previous;
+        }
+
+        finalNode.distance += delta;
+        maxLength = Math.max(finalNode.distance, maxLength);
+
+        let previous = finalNode.previous;
+        while (previous != null && !pointsEqual(previous.position, current.position)) {
+          previous.distance += delta;
+          previous = previous.previous;
+        }
+      }
+      continue;
+    }
+
     visited.add(key);
-    // distances.set(key, current.distance);
 
     const cellValue = grid.get(current.position);
     const directions = cellValue === '.' ? cardinalDirections : [directionFromName(cellValue as StraightArrowDirectionName)];
     const nextCells = [...grid.adjacentFrom(current.position, {directions})].filter((next) => {
       const nextKey = pointToString(next.position);
-      return next.value !== '#' && !visited.has(nextKey); // && (distances.get(nextKey) || -1) < current.distance + 1;
+      return next.value !== '#' && !visited.has(nextKey);
     });
     const branch = nextCells.length > 1;
     for (const next of nextCells) {
-      open.push({
+      const nextNode = {
         position: next.position,
         distance: current.distance + 1,
         visited: branch ? new Set(visited) : visited,
-      });
+        previous: current,
+      };
+      open.push(nextNode);
     }
   }
   return maxLength;
