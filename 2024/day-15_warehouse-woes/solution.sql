10e4176280a436ff92d5188b4c8b5a7326d38000
Lennart Hensler <lennart@hensler.email>
2024-12-17 01:19:33 +0100

    2024 Day 15 Part 2: works but slow (~4 mins)

--- 2024/day-15_warehouse-woes/solution.sql
+++ 2024/day-15_warehouse-woes/solution.sql
@@ -11,6 +11,7 @@ SET VARIABLE example = '
     <^^>>>vv<v>>v<<
 ';
 SET VARIABLE exampleSolution1 = 2028;
+SET VARIABLE exampleSolution2 = NULL;
 
 SET VARIABLE example = '
     ##########
@@ -36,14 +37,14 @@ SET VARIABLE example = '
     v^^>>><<^^<>>^v^<v^vv<>v^<<>^<^v^v><^<<<><<^<v><v<>vv>>v><v^<vv<>v^<<^
 ';
 SET VARIABLE exampleSolution1 = 10092;
-SET VARIABLE exampleSolution2 = NULL;
+SET VARIABLE exampleSolution2 = 9021;
 
 CREATE OR REPLACE VIEW example AS SELECT trim(getvariable('example'), chr(10) || ' ') as text;
 
 CREATE OR REPLACE TABLE input AS
 SELECT trim(content, chr(10) || ' ') as text FROM read_text('input');
 SET VARIABLE solution1 = 1465152;
-SET VARIABLE solution2 = NULL;
+SET VARIABLE solution2 = 1511259;
 
 -- SET VARIABLE mode = 'example';
 SET VARIABLE mode = 'input';
@@ -59,7 +60,7 @@ CREATE OR REPLACE VIEW input_parts AS (
     )
 );
 
-CREATE OR REPLACE VIEW warehouse AS (
+CREATE OR REPLACE TABLE warehouse AS (
     WITH
         lines AS (
             SELECT regexp_split_to_table(warehouse, '\n\s*') as line
@@ -121,7 +122,7 @@ CREATE OR REPLACE TABLE updated_warehouse AS (
                         WHERE w.object = '@'
                     ),
                     moved_objects AS (
-                        -- Empty Space:
+                        -- Empty Space
                         SELECT
                             unnest(['@', '.']) as object,
                             unnest([r.next_y, r.idy]) as idy,
@@ -129,7 +130,7 @@ CREATE OR REPLACE TABLE updated_warehouse AS (
                         FROM robot r
                         JOIN updated_warehouse e ON e.object = '.' AND e.idy = r.next_y AND e.idx = r.next_x
                         UNION ALL
-                        -- Pushable Box:
+                        -- Pushable Box
                         SELECT
                             unnest(['@', '.', 'O']) as object,
                             unnest([r.next_y, r.idy, ab.idy]) as idy,
@@ -166,7 +167,140 @@ CREATE OR REPLACE TABLE updated_warehouse AS (
     WHERE idm = (SELECT count() FROM moves)
 );
 
-CREATE OR REPLACE TABLE results AS (
+CREATE OR REPLACE TABLE scaled_warehouse AS (
+    WITH
+        scaled_parts AS (
+            SELECT
+                replace(warehouse, '#', '##') as walls,
+                replace(walls, 'O', '[]') as boxes,
+                replace(boxes, '.', '..') as floors,
+                replace(floors, '@', '@.') as scaled,
+            FROM input_parts
+        ),
+        lines AS (
+            SELECT regexp_split_to_table(scaled, '\n\s*') as line
+            FROM scaled_parts
+        )
+
+    SELECT
+        idy - 1 as idy,
+        generate_subscripts(objects, 1) - 1 as idx,
+        unnest(objects) as object,
+    FROM (
+        SELECT
+            row_number() OVER () as idy,
+            string_split(line, '') as objects
+        FROM lines
+    )
+);
+
+CREATE OR REPLACE TABLE updated_scaled_warehouse AS (
+    WITH RECURSIVE
+        updated_warehouse AS (
+            SELECT
+                0 as idm,
+                idy,
+                idx,
+                object,
+            FROM scaled_warehouse
+            UNION ALL (
+                WITH RECURSIVE
+                    robot AS (
+                        SELECT
+                            w.idy, w.idx,
+                            m.dir,
+                            d.dy, d.dx,
+                            w.idy + d.dy as next_y,
+                            w.idx + d.dx as next_x,
+                        FROM updated_warehouse w
+                        JOIN moves m ON m.idm = w.idm + 1
+                        JOIN directions d USING (dir)
+                        WHERE w.object = '@'
+                    ),
+                    vertical_boxes AS (
+                        SELECT
+                            '@' as object,
+                            r.idy,
+                            r.idx,
+                            r.dy,
+                        FROM robot r
+                        WHERE r.dx = 0
+                        UNION ALL
+                        SELECT
+                            bb.object,
+                            bb.idy,
+                            bb.idx,
+                            v.dy,
+                        FROM vertical_boxes v
+                        JOIN updated_warehouse b ON b.object IN ('[', ']', '#') AND b.idy = v.idy + v.dy AND b.idx = v.idx
+                        JOIN updated_warehouse bb ON (bb.object = ']' AND bb.idy = b.idy AND bb.idx = b.idx + 1) OR
+                                                     (bb.object = '[' AND bb.idy = b.idy AND bb.idx = b.idx - 1) OR
+                                                     (bb.idy = b.idy AND bb.idx = b.idx)
+                        -- Break after first encountered wall
+                        WHERE NOT EXISTS (FROM vertical_boxes WHERE "object" = '#')
+                    ),
+                    movable_vertical_boxes AS (
+                        -- Drop all records if no box or any wall was encountered
+                        FROM vertical_boxes
+                        WHERE EXISTS (FROM vertical_boxes v WHERE v.object IN ('[', ']')) AND
+                              NOT EXISTS (FROM vertical_boxes v WHERE "object" = '#')
+                    ),
+                    moved_objects AS (
+                        -- Empty Space
+                        SELECT
+                            unnest(['@', '.']) as object,
+                            unnest([r.next_y, r.idy]) as idy,
+                            unnest([r.next_x, r.idx]) as idx,
+                        FROM robot r
+                        JOIN updated_warehouse e ON e.object = '.' AND e.idy = r.next_y AND e.idx = r.next_x
+                        UNION ALL
+                        -- Movable Box (Horizontal)
+                        SELECT
+                            bb.object as object,
+                            bb.idy as idy,
+                            if(bb.object = '.', r.idx, bb.idx + r.dx) as idx,
+                        FROM robot r, (
+                            FROM updated_warehouse ab -- after box
+                            WHERE ab.object NOT IN ('[', ']') AND
+                                  ab.idy = r.idy AND ab.idx * r.dx > r.idx * r.dx
+                            ORDER BY abs(ab.idx - r.idx)
+                            LIMIT 1
+                        ) ab
+                        JOIN updated_warehouse b ON b.object IN ('[', ']') AND b.idy = r.next_y AND b.idx = r.next_x
+                        JOIN updated_warehouse bb ON bb.idy = r.idy AND bb.idx * r.dx BETWEEN r.idx * r.dx AND ab.idx * r.dx 
+                        WHERE r.dy = 0 AND ab.object = '.'
+                        UNION ALL
+                        -- Movable Box (Vertical)
+                        SELECT
+                            last(object ORDER BY object) as object,
+                            idy,
+                            idx,
+                        FROM (
+                            SELECT
+                                unnest([v.object, '.']) as object,
+                                unnest([v.idy + v.dy, v.idy]) as idy,
+                                v.idx as idx,
+                            FROM movable_vertical_boxes v
+                        )
+                        GROUP BY idy, idx
+                    )
+
+                SELECT
+                    idm + 1 as idm,
+                    idy,
+                    idx,
+                    coalesce(o.object, w.object)
+                FROM updated_warehouse w
+                LEFT JOIN moved_objects o USING (idy, idx)
+                WHERE idm < (SELECT count() FROM moves)
+            )
+        )
+
+    FROM updated_warehouse
+    WHERE idm = (SELECT count() FROM moves)
+);
+
+CREATE OR REPLACE VIEW results AS (
     WITH
         boxes AS (
             SELECT 
@@ -174,11 +308,19 @@ CREATE OR REPLACE TABLE results AS (
                 idy * 100 + idx as gps,
             FROM updated_warehouse
             WHERE idm = (SELECT count() FROM moves) AND object = 'O'
+        ),
+        scaled_boxes AS (
+            SELECT 
+                idy, idx,
+                idy * 100 + idx as gps,
+                object,
+            FROM updated_scaled_warehouse
+            WHERE idm = (SELECT count() FROM moves) AND object = '['
         )
 
     SELECT
         (SELECT sum(gps) FROM boxes) as part1,
-        NULL as part2
+        (SELECT sum(gps) FROM scaled_boxes) as part2,
 );
 
 
@@ -212,6 +354,22 @@ CREATE OR REPLACE MACRO print_move(idm) AS TABLE (
         idy,
         string_agg(object, ' ' ORDER BY idx) as line,
     FROM updated_warehouse w
+    WHERE w.idm = idm
+    GROUP BY idy
+    ORDER BY idy NULLS FIRST
+);
+
+CREATE OR REPLACE MACRO print_scaled_move(idm) AS TABLE (
+    SELECT
+        NULL as idy,
+        'Move: ' || dir as line,
+    FROM moves m
+    WHERE m.idm = idm
+    UNION ALL
+    SELECT
+        idy,
+        string_agg(object, '' ORDER BY idx) as line,
+    FROM updated_scaled_warehouse w
     WHERE w.idm = idm
     GROUP BY idy
     ORDER BY idy NULLS FIRST
