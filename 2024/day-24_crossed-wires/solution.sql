12438cf7d3ed7f8f874924debacd23fb89429889
Lennart Hensler <lennart@hensler.email>
2024-12-25 12:58:47 +0100

    2024 Day 24 Part 1: some cleanup

--- 2024/day-24_crossed-wires/solution.sql
+++ 2024/day-24_crossed-wires/solution.sql
@@ -1,18 +1,3 @@
--- SET VARIABLE example = '
---     x00: 1
---     x01: 1
---     x02: 1
---     y00: 0
---     y01: 1
---     y02: 0
-
---     x00 AND y00 -> z00
---     x01 XOR y01 -> z01
---     x02 OR y02 -> z02
--- ';
--- SET VARIABLE exampleSolution1 = 4; -- 100
--- SET VARIABLE exampleSolution2 = NULL;
-
 SET VARIABLE example = '
     x00: 1
     x01: 0
@@ -66,19 +51,6 @@ SET VARIABLE exampleSolution1 = 2024; -- 0011111101000
 SET VARIABLE exampleSolution2 = NULL;
 
 -- SET VARIABLE example = '
---     x1: 1
---     x2: 0
---     x3: 1
-
---     x1 OR x2 -> a
---     x2 AND x3 -> b
---     x1 OR a -> z1
---     a XOR b -> z2
--- ';
--- SET VARIABLE exampleSolution1 = 3; -- 11
--- SET VARIABLE exampleSolution2 = NULL;
-
--- SET VARIABLE example = '
 --     x00: 1
 --     x01: 0
 --     x02: 1
@@ -108,8 +80,8 @@ SET VARIABLE exampleSolution2 = NULL;
 
 --     tcb XOR ptp -> z03
 -- ';
--- SET VARIABLE exampleSolution1 = NULL; -- ?
--- SET VARIABLE exampleSolution2 = NULL;
+-- SET VARIABLE exampleSolution1 = 6; -- 0110
+-- SET VARIABLE exampleSolution2 = NULL; -- 1101 + 1001 = 10110 | 13 + 9 = 22
 
 CREATE OR REPLACE VIEW example AS SELECT regexp_split_to_table(trim(getvariable('example'), chr(10) || ' '), '\n\s*') as line;
 
@@ -122,7 +94,7 @@ SET VARIABLE solution2 = NULL;
 -- SET VARIABLE mode = 'example';
 SET VARIABLE mode = 'input';
 
-CREATE OR REPLACE VIEW connections AS (
+CREATE OR REPLACE VIEW gates AS (
     SELECT
         parts[1] as in1,
         parts[3] as in2,
@@ -155,14 +127,14 @@ CREATE OR REPLACE VIEW nodes AS (
     FROM initial_values
     UNION
     SELECT node
-    FROM connections
+    FROM gates
 );
 
 -- TODO Can this be done backwards?
--- - Start with output connections (value as additional column)
+-- - Start with output gates (value as additional column)
 -- - Calculate values of satisfied records
--- - Collect connections that would satisfy missing inputs
--- - Only keep connections that don't have a value or if their value is input
+-- - Collect gates that would satisfy missing inputs
+-- - Only keep gates that don't have a value or if their value is input
 --   for a connection with missing inputs
 -- - Stop when all values are set (which should happen "naturally")
 CREATE OR REPLACE VIEW propagate AS (
@@ -186,19 +158,19 @@ CREATE OR REPLACE VIEW propagate AS (
                     fuse AS (
                         SELECT any_value(terminate) FROM propagate
                     ),
-                    -- TODO If approach above doesn't work: remove "saturated" values (all connections they're an input for are satisfied)
+                    -- TODO If approach above doesn't work: remove "saturated" values (all gates they're an input for are satisfied)
                     satisfied AS (
                         SELECT
-                            c.node,
+                            g.node,
                             CASE
-                                WHEN c.op = 'AND' THEN in1.active AND in2.active
-                                WHEN c.op = 'OR' THEN in1.active OR in2.active
+                                WHEN g.op = 'AND' THEN in1.active AND in2.active
+                                WHEN g.op = 'OR' THEN in1.active OR in2.active
                                 ELSE /*XOR*/ in1.active != in2.active
                             END as active,
-                        FROM connections c, propagate in1, propagate in2
-                        WHERE c.in1 = in1.node AND c.in2 = in2.node
+                        FROM gates g, propagate in1, propagate in2
+                        WHERE g.in1 = in1.node AND g.in2 = in2.node
                         AND in1.active IS NOT NULL AND in2.active IS NOT NULL
-                        AND NOT EXISTS (FROM propagate p WHERE p.node = c.node)
+                        AND NOT EXISTS (FROM propagate p WHERE p.node = g.node)
                     ),
                     merged AS (
                         SELECT
@@ -230,123 +202,12 @@ CREATE OR REPLACE VIEW propagate AS (
                     active,
                     (FROM finished) = (FROM final_nodes) as terminate
                 FROM merged
-                WHERE NOT terminate -- FIXME terminates one step too early
-                --   AND it < 10
+                WHERE NOT terminate
             )
         )
 
     FROM propagate
-    -- WHERE terminate
-    -- delete me
-    -- ORDER BY it, node
 );
-
--- WITH RECURSIVE
---     propagate AS (
---         SELECT 
---             0 as it,
---             node,
---             active,
---         FROM initial_values
---     ),
---     propagatee AS (
---         WITH 
---             satisfied AS (
---                 SELECT
---                     c.node,
---                     CASE
---                         WHEN c.op = 'AND' THEN in1.active AND in2.active
---                         WHEN c.op = 'OR' THEN in1.active OR in2.active
---                         ELSE /*XOR*/ in1.active != in2.active
---                     END as active,
---                 FROM connections c, propagate in1, propagate in2
---                 WHERE c.in1 = in1.node AND c.in2 = in2.node
---                   AND in1.active IS NOT NULL AND in2.active IS NOT NULL
---                   AND NOT EXISTS (FROM propagate p WHERE p.node = c.node)
---             )
-            
---         SELECT
---             (SELECT any_value(it) FROM propagate) + 1 as it,
---             node,
---             active,
---         FROM (
---             FROM satisfied
---             UNION ALL
---             SELECT node, active FROM propagate
---             WHERE node[1] NOT IN ('x', 'y')
---             UNION ALL
---             FROM initial_values
---         )
---     ),
---     propagateee AS (
---         WITH 
---             satisfied AS (
---                 SELECT
---                     c.node,
---                     CASE
---                         WHEN c.op = 'AND' THEN in1.active AND in2.active
---                         WHEN c.op = 'OR' THEN in1.active OR in2.active
---                         ELSE /*XOR*/ in1.active != in2.active
---                     END as active,
---                 FROM connections c, propagatee in1, propagatee in2
---                 WHERE c.in1 = in1.node AND c.in2 = in2.node
---                   AND in1.active IS NOT NULL AND in2.active IS NOT NULL
---                   AND NOT EXISTS (FROM propagatee p WHERE p.node = c.node)
---             )
-            
---         SELECT
---             (SELECT any_value(it) FROM propagatee) + 1 as it,
---             node,
---             active,
---         FROM (
---             FROM satisfied
---             UNION ALL
---             SELECT node, active FROM propagatee
---             WHERE node[1] NOT IN ('x', 'y')
---             UNION ALL
---             FROM initial_values
---         )
---     ),
---     propagateeee AS (
---         WITH 
---             satisfied AS (
---                 SELECT
---                     c.node,
---                     CASE
---                         WHEN c.op = 'AND' THEN in1.active AND in2.active
---                         WHEN c.op = 'OR' THEN in1.active OR in2.active
---                         ELSE /*XOR*/ in1.active != in2.active
---                     END as active,
---                 FROM connections c, propagateee in1, propagateee in2
---                 WHERE c.in1 = in1.node AND c.in2 = in2.node
---                   AND in1.active IS NOT NULL AND in2.active IS NOT NULL
---                   AND NOT EXISTS (FROM propagateee p WHERE p.node = c.node)
---             )
-            
---         SELECT
---             (SELECT any_value(it) FROM propagateee) + 1 as it,
---             node,
---             active,
---         FROM (
---             FROM satisfied
---             UNION ALL
---             SELECT node, active FROM propagateee
---             WHERE node[1] NOT IN ('x', 'y')
---             UNION ALL
---             FROM initial_values
---         )
---     )
-
--- FROM propagate
--- UNION ALL
--- FROM propagatee
--- UNION ALL
--- FROM propagateee
--- UNION ALL
--- FROM propagateeee
--- ORDER BY it, node
--- ;
-
 
 CREATE OR REPLACE VIEW output AS (
     SELECT
