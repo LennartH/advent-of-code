59f8069646aeb7186c0fbdaceb587cc4178ee971
Lennart Hensler <lennart@hensler.email>
2025-01-16 21:41:28 +0100

    2024 Day 4: more fiddling

--- 2024/day-04_ceres-search/solution.sql
+++ 2024/day-04_ceres-search/solution.sql
@@ -20,8 +20,8 @@ SET VARIABLE solution1 = 2633;
 SET VARIABLE solution2 = 1936;
 
 .maxrows 75
-SET VARIABLE mode = 'example';
--- SET VARIABLE mode = 'input';
+-- SET VARIABLE mode = 'example';
+SET VARIABLE mode = 'input';
 
 CREATE OR REPLACE VIEW tokens AS (
     SELECT
@@ -66,24 +66,48 @@ CREATE OR REPLACE VIEW tokens AS (
 --     WHERE slice = 'XMAS'
 -- );
 
-CREATE OR REPLACE TABLE x_mas_count AS (
+CREATE OR REPLACE VIEW x_mas_count AS (
     WITH
-        a_corners AS (
+        -- -- ~1.25s
+        -- corners_join AS (
+        --     FROM tokens t, tokens c1, tokens c2, tokens c3, tokens c4
+        --     SELECT t.token -- DISTINCT t.idx, t.idy
+        --     WHERE t.token = 'A'
+        --       AND c1.idx - t.idx = -1 AND c1.idy - t.idy = -1 AND (c1.token = 'S' OR c1.token = 'M')
+        --       AND c2.idx - t.idx =  1 AND c2.idy - t.idy = -1 AND (c2.token = 'S' OR c2.token = 'M')
+        --       AND c3.idx - t.idx =  1 AND c3.idy - t.idy =  1 AND (c3.token = 'S' OR c3.token = 'M')
+        --       AND c4.idx - t.idx = -1 AND c4.idy - t.idy =  1 AND (c4.token = 'S' OR c4.token = 'M')
+        --       AND c1.token != c3.token AND c2.token != c4.token
+        -- )
+
+        -- -- ~5s
+        -- corners_subquery AS (
+        --     FROM tokens t
+        --     SELECT
+        --         (FROM tokens c1 SELECT c1.token WHERE c1.idx - t.idx = -1 AND c1.idy - t.idy = -1) as c1,
+        --         (FROM tokens c2 SELECT c2.token WHERE c2.idx - t.idx =  1 AND c2.idy - t.idy = -1) as c2,
+        --         (FROM tokens c3 SELECT c3.token WHERE c3.idx - t.idx =  1 AND c3.idy - t.idy =  1) as c3,
+        --         (FROM tokens c4 SELECT c4.token WHERE c4.idx - t.idx = -1 AND c4.idy - t.idy =  1) as c4,
+        --     WHERE t.token = 'A' AND c1 != c3 AND c2 != c4
+        --       AND (c1 = 'S' OR c1 = 'M') AND (c2 = 'S' OR c2 = 'M')
+        --       AND (c3 = 'S' OR c3 = 'M') AND (c4 = 'S' OR c4 = 'M')
+        -- )
+
+        corners_groupby AS (
             FROM tokens t
-            LEFT JOIN tokens n ON abs(t.idx - n.idx) = 1 AND abs(t.idy - n.idy) = 1
+            JOIN tokens n ON abs(t.idx - n.idx) = 1 AND abs(t.idy - n.idy) = 1
             SELECT
-                n.token as corner,
-                n.idx as x,
-                n.idy as y,
-            -- TODO opposite corners have to be different
-            WHERE t.token = 'A' --AND n.token IN ('M', 'S') 
-            -- GROUP BY t.idx, t.idy, t.token -- TODO use single id instead
-            -- HAVING count() FILTER (n.token = 'M') = 2 AND count() FILTER (n.token = 'S') = 2
+                t.idx, t.idy,
+                list(n.token ORDER BY n.idy ASC, n.idx ASC) as corners,
+            WHERE t.token = 'A'
+            GROUP BY t.idx, t.idy
+            HAVING corners[1] != corners[3] AND corners[2] != corners[4] AND
+                   list_sort(corners) = ['M', 'M', 'S', 'S']
         )
     
     SELECT count() as count
-    FROM boxes
-    WHERE box SIMILAR TO 'M.M.A.S.S|M.S.A.M.S|S.S.A.M.M|S.M.A.S.M'
+    -- FROM a_corners
+    FROM corners_groupby
 );
 
 CREATE OR REPLACE VIEW results AS (
