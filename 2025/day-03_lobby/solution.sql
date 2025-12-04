d4f2231a888717cfa46e2f10d0519b0c9d9602b2
Lennart Hensler <lennart@hensler.email>
2025-12-04 07:13:50 +0100

    2025 Day 3 (SQL): cleanup

--- 2025/day-03_lobby/solution.sql
+++ 2025/day-03_lobby/solution.sql
@@ -17,89 +17,72 @@ SET VARIABLE solution2 = 170731717900423;
 SET VARIABLE mode = 'example';
 -- SET VARIABLE mode = 'input';
 
-CREATE OR REPLACE TABLE batteries AS (
+CREATE OR REPLACE TABLE battery_banks AS (
     FROM query_table(getvariable('mode'))
     SELECT
-           bank_id: row_number() OVER (),
-        battery_id: generate_subscripts(string_split(line, ''), 1),
-        battery_value: unnest(string_split(line, ''))::INTEGER,
+          bank_id: row_number() OVER (),
+        batteries: split(line, ''),
 );
 
 CREATE OR REPLACE TABLE largest_bank_joltages AS (
-    WITH
-        largest_battery AS (
-            FROM batteries
-            SELECT
-                bank_id,
-                largest_battery_id: arg_max(battery_id, battery_value ORDER BY battery_id),
-                largest_battery_value: max(battery_value ORDER BY battery_id),
-            GROUP BY
-                bank_id
-        ),
-        largest_before_after AS (
-            FROM largest_battery l
-            LEFT JOIN batteries a ON a.bank_id = l.bank_id AND a.battery_id > l.largest_battery_id
-            LEFT JOIN batteries b ON b.bank_id = l.bank_id AND b.battery_id < l.largest_battery_id
-            SELECT
-                l.bank_id,
-                largest_battery_id,
-                largest_battery_value,
-                largest_battery_value_after: max(a.battery_value),
-                largest_battery_value_before: max(b.battery_value),
-            GROUP BY ALL
-        )
-
-    FROM largest_before_after
-    SELECT
-        bank_id,
-        joltage: 10 * if(largest_battery_value_after IS NULL, largest_battery_value_before, largest_battery_value)
-            + if(largest_battery_value_after IS NULL, largest_battery_value, largest_battery_value_after),
-);
-
-CREATE OR REPLACE TABLE largest_bank_joltages_safety_override AS (
     WITH RECURSIVE
-        banks AS (
-            FROM batteries
-            SELECT
-                bank_id,
-                batteries: list(battery_value ORDER BY battery_id),
-            GROUP BY ALL
-        ),
         largest_sequence AS (
-            FROM banks
-            SELECT
-                *,
-                battery_count: length(batteries),
-                remaining: 12,
-                battery_from: 1,
-                largest: []::INTEGER[],
+            FROM (
+                FROM battery_banks
+                SELECT
+                             bank_id,
+                           batteries,
+                        battery_from: 1,
+                    battery_sequence: []::STRING[],
+                           remaining: 2,
+                      used_batteries: 2,
+                UNION ALL
+                FROM battery_banks
+                SELECT
+                             bank_id,
+                           batteries,
+                        battery_from: 1,
+                    battery_sequence: []::STRING[],
+                           remaining: 12,
+                      used_batteries: 12,
+            )
             UNION ALL
-            FROM largest_sequence
+            FROM (
+                FROM largest_sequence
+                SELECT
+                             bank_id, batteries,
+                       battery_count: length(batteries),
+                           remaining: remaining - 1,
+                               slice: batteries[battery_from : battery_count - remaining + 1],
+                    largest_in_slice: list_max(slice),
+                        battery_from: list_indexof(slice, largest_in_slice) + battery_from,
+                    battery_sequence: list_append(battery_sequence, largest_in_slice),
+                      used_batteries,
+            )
             SELECT
                 bank_id,
-                batteries,
-                battery_count,
-                remaining: remaining - 1,
-                battery_from: list_indexOf(
-                    batteries[battery_from : battery_count - remaining + 1],
-                    list_max(batteries[battery_from : battery_count - remaining + 1])
-                ) + battery_from,
-                largest: list_append(largest, list_max(batteries[battery_from : battery_count - remaining + 1])),
+                batteries, 
+                battery_from, 
+                battery_sequence, 
+                remaining, 
+                used_batteries,
             WHERE
-                remaining > 0
+                remaining >= 0
         )
     
     FROM largest_sequence
     SELECT
-        bank_id,
-        joltage: aggregate(largest, 'string_agg', '')::BIGINT,
+               bank_id,
+               joltage: aggregate(battery_sequence, 'string_agg', '')::BIGINT,
+        used_batteries,
     WHERE remaining = 0
 );
 
 CREATE OR REPLACE VIEW results AS (
+    FROM largest_bank_joltages
     SELECT
-        part1: (FROM largest_bank_joltages SELECT sum(joltage)),
-        part2: (FROM largest_bank_joltages_safety_override SELECT sum(joltage)),
+        part1: sum(joltage) FILTER (used_batteries = 2),
+        part2: sum(joltage) FILTER (used_batteries = 12),
 );
 
 
