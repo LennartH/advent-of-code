ec4ae2e67c6109dc44a1841d189e527d91a1b0ce
Lennart Hensler <lennart@hensler.email>
2025-12-06 22:56:32 +0100

    2025 Day 6 Part 2 (SQL): original solution with duckdb for part 2

--- 2025/day-06_trash-compactor/solution.sql
+++ 2025/day-06_trash-compactor/solution.sql
@@ -1,34 +1,33 @@
 SET VARIABLE example = '
-    123 328  51 64 
-     45 64  387 23 
-      6 98  215 314
-    *   +   *   +  
+123 328  51 64 
+ 45 64  387 23 
+  6 98  215 314
+*   +   *   +  
 ';
 SET VARIABLE exampleSolution1 = 4277556;
-SET VARIABLE exampleSolution2 = NULL;
-CREATE OR REPLACE VIEW example AS SELECT regexp_split_to_table(trim(getvariable('example'), E'\n '), '\n\s*') as line;
+SET VARIABLE exampleSolution2 = 3263827;
+CREATE OR REPLACE VIEW example AS SELECT regexp_split_to_table(trim(getvariable('example'), E'\n'), '\n') as line;
 
 CREATE OR REPLACE TABLE input AS
 FROM read_text('input') SELECT regexp_split_to_table(trim(content, E'\n '), '\n\s*') as line;
 SET VARIABLE solution1 = 7644505810277;
-SET VARIABLE solution2 = NULL;
+SET VARIABLE solution2 = 12841228084455;
 
 .maxrows 75
 SET VARIABLE mode = 'example';
 -- SET VARIABLE mode = 'input';
 
-CREATE OR REPLACE TABLE parser AS (
-    FROM query_table(getvariable('mode'))
-    SELECT
-        y: row_number() OVER (),
-        x: generate_subscripts(string_split_regex(trim(line), '\s+'), 1),
-        value: unnest(string_split_regex(trim(line), '\s+')),
-);
-
-CREATE OR REPLACE TABLE worksheet_results AS (
+CREATE OR REPLACE TABLE left_to_right_parser AS (
     WITH
+        values AS (
+            FROM query_table(getvariable('mode'))
+            SELECT
+                y: row_number() OVER (),
+                x: generate_subscripts(string_split_regex(trim(line), '\s+'), 1),
+                value: unnest(string_split_regex(trim(line), '\s+')),
+        ),
         transposed AS (
-            FROM parser
+            FROM values
             SELECT
                 id: x,
                 entries: list(value),
@@ -51,10 +50,59 @@ CREATE OR REPLACE TABLE worksheet_results AS (
         END
 );
 
+CREATE OR REPLACE TABLE top_to_bottom_parser AS (
+    WITH
+        symbols AS (
+            FROM query_table(getvariable('mode'))
+            SELECT
+                y: row_number() OVER (),
+                x: generate_subscripts(string_split_regex(line, ''), 1),
+                symbol: unnest(string_split_regex(line, '')),
+        ),
+        columns AS (
+            FROM symbols
+            SELECT
+                x,
+                value: string_agg(nullif(symbol, ' '), '' ORDER BY y),
+            GROUP BY x
+        ),
+        extract_operator AS (
+            FROM columns
+            SELECT
+                x,
+                operator: if(value[-1] IN ('+', '*'), value[-1], NULL),
+                value: if(value[-1] IN ('+', '*'), value[:-2], value)::BIGINT,
+        ),
+        grouped AS (
+            FROM extract_operator
+            SELECT
+                id: 1 + sum(if(value IS NULL, 1, 0)) OVER (ORDER BY x ROWS UNBOUNDED PRECEDING),
+                operator,
+                value,
+            QUALIFY value IS NOT NULL
+        ),
+        problems AS (
+            FROM grouped
+            SELECT
+                id,
+                operator: any_value(operator),
+                values: list(value),
+            GROUP BY id
+        )
+
+    FROM problems
+    SELECT
+        id,
+        result: CASE operator
+            WHEN '+' THEN list_sum(values)::BIGINT
+            WHEN '*' THEN list_product(values)::BIGINT
+        END
+);
+
 CREATE OR REPLACE VIEW results AS (
     SELECT
-        part1: (FROM worksheet_results SELECT sum(result)),
-        part2: NULL,
+        part1: (FROM left_to_right_parser SELECT sum(result)),
+        part2: (FROM top_to_bottom_parser SELECT sum(result)),
 );
 
 
